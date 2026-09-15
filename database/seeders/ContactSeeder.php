8c8aa7b35d9258095ef7b660b57ef32ae36c7269
Kazuki Yamaoka <kazuki07y@gmail.com>
2026-09-16 00:27:24 +0900

    feat: fix migrate #11

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Contact;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class ContactSeeder extends Seeder
@@ -12,11 +13,16 @@ class ContactSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // ★ データベースからタグを取得しておく
+        $tags = Tag::all();
+
+        // タグが存在する場合にランダムに紐付ける
         Contact::factory()->count(20)->create()->each(function ($contact) use ($tags) {
-            $contact->tags()->attach(
-                $tags->random(3)->pluck('id')
-            );
+            if ($tags->isNotEmpty()) {
+                $contact->tags()->attach(
+                    $tags->random(min(3, $tags->count()))->pluck('id')
+                );
+            }
         });
     }
 }
